fec5b3d63f14f3d4b1551ea9b45f7b07652f0a3f
Mateusz <drozdz91@interia.pl>
2020-04-26 14:10:26 +0200

    Added working solution for adding screenshots to allure report

--- src/main/java/general/TestBase.java
+++ src/main/java/general/TestBase.java
@@ -1,17 +1,17 @@
 package general;
 
-import io.qameta.allure.Attachment;
 import io.qameta.allure.Step;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import utils.ScreenshotMaker;
 
 import static general.PropertyManager.getInstance;
 import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.testng.ITestResult.FAILURE;
+import static org.testng.ITestResult.SUCCESS_PERCENTAGE_FAILURE;
 
 public class TestBase {
 
@@ -29,15 +29,14 @@ public class TestBase {
     @Step("Disposing browser")
     @AfterMethod
     public void closeBrowser(ITestResult result) {
-        if (result.getStatus() == ITestResult.FAILURE || result.getStatus() == ITestResult.SUCCESS_PERCENTAGE_FAILURE)
-            makeScreenShot(result);
+        makeScreenshotOnTestFailure(result);
         driver.close();
         driver.quit();
     }
 
-    @Attachment(value = "Page screenshot test failure", type = "image/png")
-    public byte[] makeScreenShot(ITestResult result) {
-        byte[] screenshotFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
-        return screenshotFile;
+    private void makeScreenshotOnTestFailure(ITestResult result) {
+        ScreenshotMaker screenshotMaker = new ScreenshotMaker();
+        if (result.getStatus() == FAILURE || result.getStatus() == SUCCESS_PERCENTAGE_FAILURE)
+            screenshotMaker.makeScreenshot(driver);
     }
 }
